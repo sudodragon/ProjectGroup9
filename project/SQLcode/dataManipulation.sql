9e9185b852052686c63300b6a58e44ebbd048adc
FatalVindicator <bamarandino@gmail.com>
2021-02-18 12:40:43 -0500

    Update dataManipulation.sql

--- project/SQLcode/dataManipulation.sql
+++ project/SQLcode/dataManipulation.sql
@@ -17,3 +17,28 @@ INSERT INTO missions (directive, status, location)
 VALUES (#directive, #status, #location);
 
 SELECT * FROM missions;
+
+-- Personnel
+INSERT INTO personnel (firstName, lastName)
+VALUES (#firstName, #lastName);
+
+UPDATE personnel
+SET firstName = #firstName, lastName = #lastName, rankID = (SELECT rankID FROM ranks WHERE rankName = #rankName), 
+    shipID = (SELECT shipID FROM ships WHERE shipName = # shipName)
+
+DELETE FROM personnel WHERE firstName = #firstName and lastName = #lastName;
+
+SELECT * FROM ranks;
+
+-- Duties
+INSERT INTO duties (dutyName, priority, responsibilities)
+VALUES (#dutyName, #priority, #responsibilities);
+
+SELECT * FROM duties;
+
+-- PersonnelDuties
+INSERT INTO personnel_duties (personnelID, dutyID)
+VALUES ((SELECT personnelID FROM personnel WHERE firstName = #firstName and lastName = #lastName),
+    (SELECT dutyID FROM duties WHERE dutyName = #dutyName));
+
+SELECT * FROM personnel_duties;
