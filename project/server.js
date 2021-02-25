dd80480ba18d334e51f2481376d0c4b3c27fdc96
sudodragon <49923950+sudodragon@users.noreply.github.com>
2021-02-25 15:28:41 -0500

    Update server.js

--- project/server.js
+++ project/server.js
@@ -1,5 +1,3 @@
-//This is SUPER in progress and doesn't fully work yet
-
 let express = require('express');
 let exphbs = require('express-handlebars').create({
     defaultLayout:'main'
@@ -15,71 +13,13 @@ app.set('view engine', 'handlebars');
 app.use(bodyparser.urlencoded({extended:true}));
 app.use('/static', express.static('public'));
 app.set('mysql', mysql);
-//app.use('/duties', require('./duties.js'));
-//app.use('/missions', require('./missions.js'));
-//app.use('/personnel', require('./personnel.js'));
-//app.use('/personToDuty', require('/.personToDuty.js'));
+app.use('/duties', require('./duties.js'));
+app.use('/missions', require('./missions.js'));
+app.use('/personnel', require('./personnel.js'));
+app.use('/personToDuty', require('./personToDuty.js'));
 app.use('/ranks', require('./ranks.js'));
-//app.use('/ships', require('./ships.js'));
+app.use('/ships', require('./ships.js'));
 app.use('/', express.static('public'));
-
-app.get('/duties', (req, res) => {
-    res.render('duties');
-});
-
-app.get('/missions', (req, res) => {
-    let context = {};
-    let missions = {
-        missionID: "", 
-        directive : "", 
-        status : "", 
-        location : ""
-    };
-
-    //sql stuff here
-
-    context.missions = missions;
-    res.render('missions', context);
-});
-
-app.get('/personnel', (req, res) => {
-    res.render('personnel');
-});
-
-app.get('/personToDuty', (req, res) => {
-    res.render('personToDuty');
-});
-/*
-app.get('/ranks', (req, res) => {
-    let context = {};
-    let ranks = {
-        rankId: "", 
-        rank : "", 
-        pay : "", 
-        minYears : ""
-    };
-
-    //sql stuff here
-
-    context.ranks = ranks;
-    res.render('ranks', context);
-});
-*/
-app.get('/ships', (req, res) => {
-    let context = {};
-    let ships = {
-        shipName: "", 
-        registry : "", 
-        class : "", 
-        location : "",
-        missionId : ""
-    };
-
-    //sql stuff here
-    
-    context.ships = ships;
-    res.render('ships', context);
-});
 
 app.get('/', (req, res) => {
     res.render('home');
