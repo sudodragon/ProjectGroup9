5fafecb7ad835f55c842bc62e0240dd2f5a3c926
FatalVindicator <bamarandino@gmail.com>
2021-03-03 16:15:01 -0500

    personToDuty delete done

--- project/personToDuty.js
+++ project/personToDuty.js
@@ -27,21 +27,37 @@ module.exports = function () {
         }
     });
     
-        router.post('/', function(req, res){
-            let mysql = req.app.get('mysql');
-            let [firstName, lastName, dutyName] = [req.body.firstName, req.body.lastName, req.body.dutyName]
-            let sql = "INSERT INTO personnel_duties (personnelID, dutyID) VALUES ((SELECT personnelID FROM personnel WHERE firstName = \'" + firstName + "\' and lastName = \'" + lastName + "\'), (SELECT dutyID FROM duties WHERE dutyName = \'" + dutyName + "\'))";
-            let inserts = [req.body.firstName, req.body.lastName, req.body.dutyName];
-            sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-                if(error){
-                    console.log(JSON.stringify(error))
-                    res.write(JSON.stringify(error));
-                    res.end();
-                }else{
-                    res.redirect('/personToDuty');
-                }
-            });
+    router.post('/delete', function(req, res){
+        let mysql = req.app.get('mysql');
+        let [personnelID, dutyID] = [req.body.personnelID, req.body.dutyID]
+        let sql = "DELETE FROM personnel_duties WHERE personnelID = \'" + personnelID + "\' and dutyID = \'" + dutyID + "\'";
+        let inserts = [req.body.personnelID, req.body.dutyID];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/personToDuty');
+            }
         });
+    });
+
+    router.post('/', function(req, res){
+        let mysql = req.app.get('mysql');
+        let [firstName, lastName, dutyName] = [req.body.firstName, req.body.lastName, req.body.dutyName]
+        let sql = "INSERT INTO personnel_duties (personnelID, dutyID) VALUES ((SELECT personnelID FROM personnel WHERE firstName = \'" + firstName + "\' and lastName = \'" + lastName + "\'), (SELECT dutyID FROM duties WHERE dutyName = \'" + dutyName + "\'))";
+        let inserts = [req.body.firstName, req.body.lastName, req.body.dutyName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/personToDuty');
+            }
+        });
+    });
     
     return router;
 }();
