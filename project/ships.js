4e6b6949c7e00b2bf8fe2548e6e6cd81974fd3de
FatalVindicator <bamarandino@gmail.com>
2021-03-09 15:23:12 -0500

    ships insert has missions directive drop down

--- project/ships.js
+++ project/ships.js
@@ -3,7 +3,7 @@ module.exports = function() {
     let router = express.Router();
     
     function getShips(res, mysql, context, complete) {
-        let query = "SELECT shipId, shipName, registry, class, currentLocation, missionId FROM ships";
+        let query = "SELECT shipId, shipName, registry, class, currentLocation, directive FROM ships LEFT JOIN missions ON ships.missionID = missions.missionID";
         mysql.pool.query(query, (error, results, fields) => {
             if (error) {
                 res.write(JSON.stringify(error));
@@ -13,15 +13,32 @@ module.exports = function() {
             complete();
         });
     }
+
+    function getMissions(res, mysql, context, complete) {
+        let query = "SELECT missionId, directive FROM missions";
+
+        mysql.pool.query(query, (error, results, fields) => {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.missions = results;
+            complete();
+        });
+    }
+
+
     router.get('/', (req, res) => {
         let callbackCount = 0;
         let context = {};
         let mysql = req.app.get('mysql');
         getShips(res, mysql, context, complete);
-
+        getMissions(res, mysql, context, complete);
+        
         function complete(){
             callbackCount++;
-            if (callbackCount >= 1) {
+            if (callbackCount >= 2) {
+                console.log(context)
                 res.render('ships', context);
             }
         }      
@@ -31,8 +48,8 @@ module.exports = function() {
 
     router.post('/', function(req, res){
         let mysql = req.app.get('mysql');
-        let sql = "INSERT INTO ships (shipName, registry, class, currentLocation) VALUES (?,?,?,?)";
-        let inserts = [req.body.shipName, req.body.registryNumber, req.body.class, req.body.currentLocation];
+        let sql = "INSERT INTO ships (shipName, registry, class, currentLocation, missionId) VALUES (?,?,?,?,?)";
+        let inserts = [req.body.shipName, req.body.registryNumber, req.body.class, req.body.currentLocation, req.body.missionId];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
