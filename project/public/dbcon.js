57483af77ec2f70497807cd876d76df1a4399394
Alyssa Lada <ladaa@flip2.engr.oregonstate.edu>
2021-03-15 09:54:37 -0700

    delete unused files

--- project/public/dbcon.js
+++
@@ -1,9 +0,0 @@
-let mysql = require('mysql');
-let pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_ladaa',
-  password        : '9140',
-  database        : 'cs340_ladaa'
-});
-module.exports.pool = pool;
